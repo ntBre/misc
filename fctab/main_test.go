bef10fbbc97fdb420ee8a2dc8aac3acb9f8fff9a
Brent Westbrook <brentrwestbrook@gmail.com>
2020-07-23 17:00:12 -0500

    fctab: switch to table based tests for Read{15,30,40}

--- fctab/main_test.go
+++ fctab/main_test.go
@@ -6,17 +6,37 @@ import (
 )
 
 func TestRead15(t *testing.T) {
-	got := Read15("testfiles/small.15")
-	want := []string{
-		"$F_{1,1}$ & 0.7104237500",
-		"$F_{1,2}$ & -0.0000001512",
-		"$F_{1,3}$ & -0.0000000252",
-		"$F_{1,4}$ & -0.0625424581",
-		"$F_{1,5}$ & -0.0000000980",
-		"$F_{1,6}$ & 0.0000001456",
+	tests := []struct {
+		file string
+		want []string
+	}{
+		{
+			file: "testfiles/small.15",
+			want: []string{
+				"$F_{1,1}$ & 0.7104237500",
+				"$F_{1,2}$ & -0.0000001512",
+				"$F_{1,3}$ & -0.0000000252",
+				"$F_{1,4}$ & -0.0625424581",
+				"$F_{1,5}$ & -0.0000000980",
+				"$F_{1,6}$ & 0.0000001456",
+			},
+		},
+		{
+			file: "testfiles/small5.15",
+			want: []string{
+				"$F_{1,1}$ & 0.7104237500",
+				"$F_{1,2}$ & -0.0000001512",
+				"$F_{1,3}$ & -0.0000000252",
+				"$F_{1,4}$ & -0.0625424581",
+				"$F_{1,5}$ & -0.0000000980",
+			},
+		},
 	}
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v, wanted %v\n", got, want)
+	for _, test := range tests {
+		got := Read15(test.file)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("got %v, wanted %v\n", got, test.want)
+		}
 	}
 }
 
@@ -29,17 +49,37 @@ func TestDimMap3(t *testing.T) {
 }
 
 func TestRead30(t *testing.T) {
-	got := Read30("testfiles/small.30")
-	want := []string{
-		"$F_{1,1,1}$ & 0.0000109630",
-		"$F_{1,1,2}$ & -1.3363443890",
-		"$F_{1,2,2}$ & 0.0000008844",
-		"$F_{2,2,2}$ & 1.3362504436",
-		"$F_{1,1,3}$ & -0.8735378010",
-		"$F_{1,2,3}$ & 0.0000028134",
+	tests := []struct {
+		file string
+		want []string
+	}{
+		{
+			file: "testfiles/small.30",
+			want: []string{
+				"$F_{1,1,1}$ & 0.0000109630",
+				"$F_{1,1,2}$ & -1.3363443890",
+				"$F_{1,2,2}$ & 0.0000008844",
+				"$F_{2,2,2}$ & 1.3362504436",
+				"$F_{1,1,3}$ & -0.8735378010",
+				"$F_{1,2,3}$ & 0.0000028134",
+			},
+		},
+		{
+			file: "testfiles/small5.30",
+			want: []string{
+				"$F_{1,1,1}$ & 0.0000109630",
+				"$F_{1,1,2}$ & -1.3363443890",
+				"$F_{1,2,2}$ & 0.0000008844",
+				"$F_{2,2,2}$ & 1.3362504436",
+				"$F_{1,1,3}$ & -0.8735378010",
+			},
+		},
 	}
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v, wanted %v\n", got, want)
+	for _, test := range tests {
+		got := Read30(test.file)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("got %v, wanted %v\n", got, test.want)
+		}
 	}
 }
 
@@ -52,16 +92,36 @@ func TestDimMap4(t *testing.T) {
 }
 
 func TestRead40(t *testing.T) {
-	got := Read40("testfiles/small.40")
-	want := []string{
-		"$F_{1,1,1,1}$ & 1.2548200557",
-		"$F_{1,1,1,2}$ & 0.0013250835",
-		"$F_{1,1,2,2}$ & 0.4185298029",
-		"$F_{1,2,2,2}$ & 0.0018503920",
-		"$F_{2,2,2,2}$ & 1.2569845853",
-		"$F_{1,1,1,3}$ & 0.0003606063",
+	tests := []struct {
+		file string
+		want []string
+	}{
+		{
+			file: "testfiles/small.40",
+			want: []string{
+				"$F_{1,1,1,1}$ & 1.2548200557",
+				"$F_{1,1,1,2}$ & 0.0013250835",
+				"$F_{1,1,2,2}$ & 0.4185298029",
+				"$F_{1,2,2,2}$ & 0.0018503920",
+				"$F_{2,2,2,2}$ & 1.2569845853",
+				"$F_{1,1,1,3}$ & 0.0003606063",
+			},
+		},
+		{
+			file: "testfiles/small5.40",
+			want: []string{
+				"$F_{1,1,1,1}$ & 1.2548200557",
+				"$F_{1,1,1,2}$ & 0.0013250835",
+				"$F_{1,1,2,2}$ & 0.4185298029",
+				"$F_{1,2,2,2}$ & 0.0018503920",
+				"$F_{2,2,2,2}$ & 1.2569845853",
+			},
+		},
 	}
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v, wanted %v\n", got, want)
+	for _, test := range tests {
+		got := Read40(test.file)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("got %v, wanted %v\n", got, test.want)
+		}
 	}
 }
